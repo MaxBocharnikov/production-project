3bb64f470d6c1a3ee1f0f9bbaac99cc984ccc82e
MaxBocharnikov <Bocharnikov3009@gmail.com>
2025-01-26 22:01:58 +0100

    add jest

--- src/shared/lib/classNames.ts
+++ src/shared/lib/classNames.ts
@@ -5,7 +5,7 @@ export function classNames(cls: string, mode: Mode = {}, additional: string[] = 
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mode)
+        ...Object.entries(mode)
             .filter(([_, value]) => Boolean(value))
             .map(([className]) => className),
     ].join(' ')
