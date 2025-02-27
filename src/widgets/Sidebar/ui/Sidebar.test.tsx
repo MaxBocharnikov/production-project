4f74abb045b75ccb12d034ecc16ffc40b61147d9
MaxBocharnikov <Bocharnikov3009@gmail.com>
2025-02-27 23:14:44 +0100

    fix tests + ui reports

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,18 +1,18 @@
 import {fireEvent, screen} from '@testing-library/react';
 import {Sidebar} from 'widgets/Sidebar';
-import {renderWithTranslation} from '../../../shared/lib/tests/renderWithTranslation';
+import {componentRender} from 'shared/config/tests/companentRender';
 
 describe("Sidebar", () => {
     test('renders', async () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
     test('sidebar non-toggled', async () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).not.toHaveClass('collapsed')
     })
     test('sidebar toggled', async () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggle = screen.getByTestId('toggle');
         fireEvent.click(toggle);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
